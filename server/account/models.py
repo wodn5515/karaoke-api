88571662613a3cf6b4e8eb25053e21470f16bbc9
wodn5515 <wodn5515@naver.com>
2021-01-06 20:16:49 +0900

    Add : account APP

---
+++ server/account/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
+
+
+# Create your models here.
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, username, email, password):
+        if not username:
+            raise ValueError("아이디를 입력해주세요.")
+        if not email:
+            raise ValueError("이메일을 입력해주세요.")
+        user = self.model(username=username, email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password):
+        user = self.create_user(username=username, email=email, password=password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    objects = UserManager()
+
+    username = models.CharField(_("ID"), max_length=20, unique=True)
+    email = models.EmailField(_("Email"), unique=True, default="")
+    is_staff = models.BooleanField(_("스태프"), default=False)
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    class Meta:
+        verbose_name = "회원"
+        verbose_name_plural = "회원"
