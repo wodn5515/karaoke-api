0d33cb04b58ed582b32827e66a3a820c3d64869f
wodn5515 <wodn5515@naver.com>
2021-01-07 20:26:49 +0900

    Modify : urls

--- server/server/urls.py
+++ server/server/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", include("frontend.urls")),
     path("api/", include("api.urls")),
     path("admin/", admin.site.urls),
 ]
