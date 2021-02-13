994675697c72f95cc94c35a13c3a9924db91ac76
wodn5515 <wodn5515@naver.com>
2021-02-13 16:47:12 +0900

    Add jwt urls

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,7 +1,14 @@
+from django.urls import path
 from rest_framework import routers
-from .views import TodoViewset
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
+#from .views import TodoViewset
 
 router = routers.DefaultRouter()
-router.register("todo", TodoViewset, "todo")
+#router.register("todo", TodoViewset, "todo")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("token/", obtain_jwt_token),
+    path("token/verify/", verify_jwt_token),
+    path("token/refresh/", refresh_jwt_token)
+]
+
