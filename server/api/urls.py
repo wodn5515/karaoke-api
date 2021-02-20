293a3bc35ea592f268506a381d97f8f7b9b49794
wodn5515 <wodn5515@naver.com>
2021-02-20 22:26:24 +0900

    url patterns 분리

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,14 +1,23 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
+from .views import tj_views, ky_views
 #from .views import TodoViewset
 
 router = routers.DefaultRouter()
 #router.register("todo", TodoViewset, "todo")
 
+tj_patterns = [
+    path("monthnew", tj_views.MonthNewView.as_view(), name="tj_monthnew"),
+    path("hitsong", tj_views.HitSongView.as_view(), name="th_hitsong")
+]
+
+ky_patterns = [
+#    path("monthnew", ky_views.monthnew, name="ky_monthnew")
+]
+
 urlpatterns = [
-    path("token/", obtain_jwt_token),
-    path("token/verify/", verify_jwt_token),
-    path("token/refresh/", refresh_jwt_token)
+    path("tj/", include(tj_patterns)),
+    path("ky/", include(ky_patterns))
 ]
 
